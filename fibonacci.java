46ba77ef7d66797bfa8a78f8b376b6bd36516c46
Vishesh Mishra <149947608+first2last@users.noreply.github.com>
2024-08-26 07:48:22 +0000

    updated

---
+++ fibonacci.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class fibonacci {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scn.nextLine();
+
+        System.out.println("Welcome, " + name);
+
+        System.out.println("Enter the number up to which you want Fibonacci numbers: ");
+        int n = scn.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci numbers up to " + n + " are:");
+
+        // Print Fibonacci numbers up to n
+        // Print the initial number
+        while (a <= n) {
+            System.out.print(a + " "); // Print the current Fibonacci number
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        scn.close();
+    }
+}
--- grade.java
+++ grade.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Grade {
+public class grade {
 
     public static void main(String[] args) {
 
